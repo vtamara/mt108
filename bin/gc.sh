1289659cb819116bce9ccef82d280f98ac29bb11
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-28 14:13:09 -0500

    Actualiza

--- bin/gc.sh
+++ bin/gc.sh
@@ -1,57 +1,67 @@
 #!/bin/sh
-# Hace pruebas, pruebas de regresión, envia a github y sube a heroku
+# Revisa errore comunes, ejecuta pruebas de regresión y del sistema y envia a github 
 
-if (test -f ".env") then {
-	. ./.env
+
+s=`grep -B 1 "^ *path" Gemfile 2> /dev/null`
+if (test "$?" = "0") then {
+  echo "Gemfile incluye gema cableada al sistema de archivos ($s)"
+  exit 1;
 } fi;
 
-grep "^ *gem *.sip.*, *path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sip cableado al sistema de archivos"
-	exit 1;
-} fi;
-grep "^ *gem *.sivel2_gen.*, *path:" Gemfile > /dev/null 2> /dev/null
-if (test "$?" = "0") then {
-	echo "Gemfile incluye un sivel2_gen cableado al sistema de archivos"
-	exit 1;
-} fi;
 grep "^ *gem *.debugger*" Gemfile > /dev/null 2> /dev/null
 if (test "$?" = "0") then {
-	echo "Gemfile incluye debugger que heroku no quiere"
+	echo "Gemfile incluye debugger"
 	exit 1;
 } fi;
-#grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
-#if (test "$?" = "0") then {
-#	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
-#	exit 1;
-#} fi;
+grep "^ *gem *.byebug*" Gemfile > /dev/null 2> /dev/null
+if (test "$?" = "0") then {
+	echo "Gemfile incluye byebug que rbx de travis-ci no quiere"
+	exit 1;
+} fi;
 
 if (test "$SINAC" != "1") then {
-  NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update
-} fi;
-if (test "$SININS" != "1") then {
-	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
-} fi;
-if (test "$SINMIG" != "1") then {
-	(bundle exec rake db:migrate sip:indices db:schema:dump)
+	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle update
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
+	( CXX=c++ yarn upgrade)
 	if (test "$?" != "0") then {
 		exit 1;
 	} fi;
 } fi;
 
-RAILS_ENV=test bundle exec rake db:drop db:setup db:migrate sip:indices
+if (test "$SININS" != "1") then {
+	NOKOGIRI_USE_SYSTEM_LIBRARIES=1 MAKE=gmake make=gmake QMAKE=qmake4 bundle install
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
+	( CXX=c++ yarn install)
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
+} fi;
+
+if (test "$SINMIG" != "1") then {
+	( bin/rails db:migrate sip:indices db:schema:dump)
+	if (test "$?" != "0") then {
+		exit 1;
+	} fi;
+} fi;
+
+( RAILS_ENV=test bin/rails db:drop db:setup; RAILS_ENV=test bin/rails db:migrate sip:indices)
 if (test "$?" != "0") then {
 	echo "No puede preparse base de prueba";
 	exit 1;
 } fi;
 
-bundle exec rails test
+CONFIG_HOSTS=www.example.com bin/rails test
 if (test "$?" != "0") then {
-	echo "No pasaron pruebas";
+	echo "No pasaron pruebas de regresion";
 	exit 1;
 } fi;
 
-RAILS_ENV=test bundle exec rake db:schema:dump
+( RAILS_ENV=test bin/rails db:schema:dump)
+
 b=`git branch | grep "^*" | sed -e  "s/^* //g"`
 git status -s
 if (test "$MENSCONS" = "") then {
@@ -62,10 +72,5 @@ git push origin ${b}
 if (test "$?" != "0") then {
 	echo "No pudo subirse el cambio a github";
 	exit 1;
-} fi;
-
-if (test "$CONH" != "") then {
-	git push heroku master
-	heroku run rake db:migrate sip:indices
 } fi;
 
