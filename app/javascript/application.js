a8f93131d3bbc6fb37025ed60dd121ed9bd258b6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-12-28 22:59:11 -0500

    Mejora javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -28,3 +28,35 @@ import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
 
+
+function establece_porcentaje(p1, p2) {
+  t1 = Math.round(200-2*(p1+p2))+10;
+  document.getElementById('vacio').setAttribute('style', 'height: ' + t1 + 
+    'px');
+  t2 = Math.round(2*p2);
+  document.getElementById('prestamo').setAttribute('style', 'height: ' + t2 + 
+    'px');
+  t3 = Math.round(2*p1);
+  document.getElementById('lleno').setAttribute('style', 'height: ' + t3 +
+    'px');
+}
+
+//document.addEventListener('turbolinks:load', function() {
+document.addEventListener('turbolinks:load', function() {
+  p = window.porcentaje;
+  s = window.suma;
+  m = window.meta;
+  pr = window.prestamo;
+  for (pi = 20; pi <= 100; pi += 20) {
+    v = Math.round(m*pi/100);
+    d = document.getElementById('e' + pi);
+    d.innerHtml = '- ' + v;
+  }
+  console.log('meta=', m);
+  console.log('suma=', s);
+  console.log('pr=', pr);
+  console.log('porcentaje=', p);
+  p2 = pr > s ? pr - s : 0
+  p2 = Math.round(p2*100/m)
+  establece_porcentaje(p, p2);
+})
